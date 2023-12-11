2d97fa0182bf2c1e9c3bb2d2ac5c132cfb967e9b
Dhiya <145046195+Dhiya16@users.noreply.github.com>
2023-12-11 22:17:50 +0530

    Update dictionary.py

--- dictionary.py
+++ dictionary.py
@@ -10,7 +10,7 @@
 #-------------------------------------------------------------------------------
 
 def generate_cubes_dictionary():
-    cubes_dict = {key: key**3 for key in range(1, 9)}
+    cubes_dict = {key: key**3 for key in range(1, 6)}
     print("Generated Dictionary:")
     print(cubes_dict)
 
