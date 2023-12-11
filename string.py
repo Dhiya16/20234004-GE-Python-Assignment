e4523a726be5da6bc0d51694cf9533cc6592c38c
Dhiya <145046195+Dhiya16@users.noreply.github.com>
2023-12-12 00:25:25 +0530

    string.py

--- string.py
+++ string.py
@@ -1,13 +1,4 @@
-#-------------------------------------------------------------------------------
-# Name:        module1
-# Purpose:
-#
-# Author:      HP
-#
-# Created:     11-12-2023
-# Copyright:   (c) HP 2023
-# Licence:     <your licence>
-#-------------------------------------------------------------------------------
+
 
 def find_frequency(char, input_string):
     frequency = input_string.count(char)
