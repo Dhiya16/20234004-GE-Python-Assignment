60f729c3ee75bf7e0a351103d270d2af8b638ffb
Dhiya <145046195+Dhiya16@users.noreply.github.com>
2023-12-12 00:30:30 +0530

    dictionary.py

--- dictionary.py
+++ dictionary.py
@@ -1,13 +1,4 @@
-#-------------------------------------------------------------------------------
-# Name:        module1
-# Purpose:
-#
-# Author:      HP
-#
-# Created:     11-12-2023
-# Copyright:   (c) HP 2023
-# Licence:     <your licence>
-#-------------------------------------------------------------------------------
+
 
 def generate_cubes_dictionary():
     cubes_dict = {key: key**3 for key in range(1, 6)}
