855e79f6f65c4b9d7eb898c16e4d9373dbbcf895
Dhiya <145046195+Dhiya16@users.noreply.github.com>
2023-12-12 00:10:05 +0530

    cubes of even integers (for loop).py

--- cubes of even integers (for loop).py
+++ cubes of even integers (for loop).py
@@ -1,13 +1,4 @@
-#-------------------------------------------------------------------------------
-# Name:        module1
-# Purpose:
-#
-# Author:      HP
-#
-# Created:     11-12-2023
-# Copyright:   (c) HP 2023
-# Licence:     <your licence>
-#-------------------------------------------------------------------------------
+
 
 def cubes_of_even_numbers_for_loop(input_list):
     result = []
